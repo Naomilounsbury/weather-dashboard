7d0ea48dd3df8d1cabae6a923d118d3fdd664034
naomi <naomilounsbury@hotmail.com>
2021-12-11 17:04:38 -0800

    gitignore

--- .history/assets/js/script_20211206093529.js
+++
@@ -1,6 +0,0 @@
-
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=82b88905657c227b366aeed2a3762dff").then(function(response) {
-  console.log("inside", response);
-});
-
-console.log("outside");
--- .history/assets/js/script_20211206094357.js
+++
@@ -1,7 +0,0 @@
-//
-fetch("api.openweathermap.org/data/2.5/weather?q=miami&appid=82b88905657c227b366aeed2a3762dff")
-.then(function(response) {
-  console.log("inside", response);
-});
-
-console.log("outside");
